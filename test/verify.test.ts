2b9013e4e79819231e1cc7300874aeafffd6972f
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-07-24 21:49:25 +0530

    first commit

---
+++ test/verify.test.ts
@@ -0,0 +1,45 @@
+import { verify, sign } from '../src';
+
+describe('verify', () => {
+  it('should verify and decode a valid token', () => {
+    const secret = 'shhhhh';
+
+    const token = sign({ payload: { name: 'TomDoesTech' }, secret });
+
+    const verified = verify({ token, secret });
+
+    expect(verified.name).toBe('TomDoesTech');
+  });
+
+  it('should throw if the signature is invalid', () => {
+    const secretOne = 'shhhhh';
+    const secretTwo = 'secretTwo';
+    const token = sign({ payload: { name: 'Tom' }, secret: secretOne });
+
+    try {
+      verify({ token, secret: secretTwo });
+    } catch (e ) {
+      const error = e as Error;
+      expect(error.message).toBe('Invalid signature');
+    }
+  });
+
+  it('should throw if the token has expired', () => {
+    const secret = 'shhhhh';
+
+    const token = sign({
+      payload: { name: 'TomDoesTech' },
+      secret,
+      options: {
+        expiresIn: -8.64e7,
+      },
+    });
+
+    try {
+      verify({ token, secret });
+    } catch (e) {
+      const error = e as Error;
+      expect(error.message).toBe('Token has expired');
+    }
+  });
+});
